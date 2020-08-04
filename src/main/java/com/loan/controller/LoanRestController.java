61aee036703f1246bffcc7d96baccaad2acbd12c
mugilanwithu@gmail.com <mugilanwithu@gmail.com>
2020-08-04 06:48:00 +0530

    Swagger Updations

--- src/main/java/com/loan/controller/LoanRestController.java
+++ src/main/java/com/loan/controller/LoanRestController.java
@@ -1,16 +1,14 @@
 package com.loan.controller;
 
 import com.loan.dao.LoanRepository;
+import com.loan.kafka.producer.UpdatePriceProducer;
 import com.loan.model.Loan;
 import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
-
-import java.awt.*;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 
 @RequestMapping(value = "loan")
@@ -19,12 +17,14 @@ public class LoanRestController {
 
     @Autowired
     private LoanRepository repository;
+    @Autowired
+    ConfigurableApplicationContext context;
 
-    @ApiOperation(value = "GetLoanData", nickname = "getLoan")
+    @ApiOperation(value = "Get Loan Details By ID", nickname = "getLoan")
 
     @RequestMapping(method = RequestMethod.GET, path = "/api/getByID", produces = "application/json")
     @ApiImplicitParams({@ApiImplicitParam(name = "id", value = "Loan Id",
-            required = false, dataType = "string", paramType = "query", defaultValue = "1")
+            required = true, dataType = "string", paramType = "query")
     })
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Success", response = Loan.class),
@@ -36,6 +36,7 @@ public class LoanRestController {
         return repository.findById(id);
     }
 
+
     @RequestMapping(method = RequestMethod.GET, path = "/api/getAll", produces = "application/json")
     @ApiImplicitParams({@ApiImplicitParam()
     })
@@ -45,7 +46,40 @@ public class LoanRestController {
             @ApiResponse(code = 403, message = "Forbidden"),
             @ApiResponse(code = 404, message = "Not Found"),
             @ApiResponse(code = 500, message = "Failure")})
-    public List getAll(@RequestParam() String id) {
-        return (List) repository.findAll();
+        public ResponseEntity<Iterable<?>> getAll() {
+        try {
+            return new ResponseEntity<Iterable<?>>(repository.findAll(), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<Iterable<?>>(HttpStatus.BAD_REQUEST);
+        }
+
     }
+
+    @RequestMapping(method = RequestMethod.POST, path = "/api/createLoan")
+    public ResponseEntity<?> addLoan(@RequestBody Loan loan) {
+        repository.save(loan);
+        return Util.createResponseEntity("Successful creation of a resource", HttpStatus.OK);
+    }
+
+
+    @RequestMapping(value = "/updateLoan", method = RequestMethod.POST)
+    public ResponseEntity<String> updateLoan(@RequestBody Loan loan) {
+
+        UpdatePriceProducer producer = context.getBean("kafkaUpdatePriceProducer", UpdatePriceProducer.class);
+        String id= loan.getId();
+        double newinterestPercentage = loan.getInterestPercentage();
+        producer.updateDataPriceAndPublishToKafka(id, newinterestPercentage,loan);
+
+        return Util.createResponseEntity("Successful updation of a resource", HttpStatus.OK);
+    }
+
 }
+
+ class Util {
+
+     public static ResponseEntity<String> createResponseEntity(String message, HttpStatus statusCode) {
+         return new ResponseEntity<>(message, statusCode);
+     }
+
+
+ }
